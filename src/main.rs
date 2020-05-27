945a62d04fadd97e570079860810586ed6e18735
eda3 <gerorin1010@gmail.com>
2020-05-27 22:12:31 +0900

    feat: 数字当てゲームの冒頭部分実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("数字当てゲーム！");
+
+    println!("1～100の間の数字を入力してください！");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("read line エラー発生");
+
+    println!("あなたの予想：{}", guess);
+
