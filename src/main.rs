2044a156b6697b143746ccf47b00df88a86f5c1d
eda3 <gerorin1010@gmail.com>
2020-05-30 22:00:32 +0900

    feat: ランダム数値と入力数値の比較を作成

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -17,6 +18,16 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("read line エラー発生");
 
+    // guessとsecret_numberを比較させるため、型変換
+    let guess: u32 = guess.trim().parse()
+        .expect("数値を入力してください");
+
     println!("あなたの予想：{}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("小さすぎます"),
+        Ordering::Greater => println!("大きすぎます"),
+        Ordering::Equal => println!("あなたの勝ちです"),
+    }
 }
 
