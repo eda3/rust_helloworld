ffe1a6da884b72a2b61e03cafaeffac8c9d10f4c
eda3 <gerorin1010@gmail.com>
2020-05-31 18:53:59 +0900

    feat: loop処理を追加

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,25 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("secret_number:{}", secret_number);
 
-    println!("1～100の間の数字を入力してください！");
+    loop {
+        println!("1～100の間の数字を入力してください！");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("read line エラー発生");
+        io::stdin().read_line(&mut guess)
+            .expect("read line エラー発生");
 
-    // guessとsecret_numberを比較させるため、型変換
-    let guess: u32 = guess.trim().parse()
-        .expect("数値を入力してください");
+        // guessとsecret_numberを比較させるため、型変換
+        let guess: u32 = guess.trim().parse()
+            .expect("数値を入力してください");
 
-    println!("あなたの予想：{}", guess);
+        println!("あなたの予想：{}", guess);
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("小さすぎます"),
-        Ordering::Greater => println!("大きすぎます"),
-        Ordering::Equal => println!("あなたの勝ちです"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さすぎます"),
+            Ordering::Greater => println!("大きすぎます"),
+            Ordering::Equal => println!("あなたの勝ちです"),
+        }
     }
 }
 
