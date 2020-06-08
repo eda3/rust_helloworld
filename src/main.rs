f57a300395fbf51b3226d42890e6804e9905d00b
eda3 <gerorin1010@gmail.com>
2020-06-08 21:16:25 +0900

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+fn main() {
+    let mut s = String::from("hello");
+    s.push_str(", world");
+    println!("{}", s);
+    println!();
+
+    /* エラーがでる。 「value borrowed here after move」
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}, world!", s1);
+    */
+
+    // clone()を使わないとエラーになる
+    let s1 = String::from("hello");
+    let _s2 = s1.clone();
+    println!("{}, world!", s1);
+    println!();
+
+    // これはエラーにならない。
+    // 整数などのプリミティブ型はスタックにすっぽり入る
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
+
+}
+
 /*
 struct Philosopher {
     name: String,
